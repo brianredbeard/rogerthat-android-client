64cdf195ec83dc089336a99615470ff24f543453
Somalias <donatogomez88@gmail.com>
2016-06-08 10:18:33 +0200

    Navigation Drawer working (no extend)

--- rogerthat/src/main/java/com/mobicage/rogerthat/NavigationView.java
+++ rogerthat/src/main/java/com/mobicage/rogerthat/NavigationView.java
@@ -1,20 +1,3 @@
-/*
- * Copyright 2016 Mobicage NV
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @@license_version:1.1@@
- */
 package com.mobicage.rogerthat;
 
 import android.content.Intent;
@@ -22,16 +5,16 @@ import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.mobicage.rogerth.at.R;
-import com.mobicage.rogerthat.plugins.messaging.MessagingActivity;
-import com.mobicage.rogerthat.util.TextUtils;
 
-public class NewsHomeActivity extends ServiceBoundActivity
+public class NavigationView extends AppCompatActivity
         implements android.support.design.widget.NavigationView.OnNavigationItemSelectedListener {
 
     ImageButton ib_hamburger, ib_newspaper, ib_shopping_basket, ib_calendar, ib_credit_car;
@@ -69,15 +52,6 @@ public class NewsHomeActivity extends ServiceBoundActivity
         ib_credit_car = (ImageButton) findViewById(R.id.nav_credit_card);
     }
 
-    @Override
-    protected void onServiceBound() {
-
-    }
-
-    @Override
-    protected void onServiceUnbound() {
-
-    }
 
 
     public void onOptionNavigationViewToolbarSelected(View v) {
@@ -90,13 +64,13 @@ public class NewsHomeActivity extends ServiceBoundActivity
                 break;
             //Launch Newspaper Activity
             case R.id.nav_newspaper:
-                intent = new Intent(NewsHomeActivity.this, ServiceFriendsActivity.class);
-                startActivity(intent);
+//                intent = new Intent(NavigationView.this, Newspaper.class);
+//                startActivity(intent);
                 break;
             //Launch Shopping Basket Activity
             case R.id.nav_shopping_basket:
-                intent = new Intent(NewsHomeActivity.this, MessagingActivity.class);
-                startActivity(intent);
+//                intent = new Intent(NavigationView.this, TermsAndConditions.class);
+//                startActivity(intent);
                 break;
             //Launch Calendar Activity
             case R.id.nav_calendar:
@@ -144,8 +118,8 @@ public class NewsHomeActivity extends ServiceBoundActivity
         Intent intent;
         switch (item.getItemId()) {
             case R.id.nav_messages:
-                intent = new Intent(NewsHomeActivity.this, ServiceFriendsActivity.class);
-                startActivity(intent);
+//                intent = new Intent(NavigationView.this, Messages.class);
+//                startActivity(intent);
                 break;
             case R.id.nav_report_card:
 //                intent = new Intent(NavigationView.this, ReportCard.class);
