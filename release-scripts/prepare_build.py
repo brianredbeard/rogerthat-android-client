aa4259e874541042d378f4f86d4e6e1fef4fc07c
Ruben Mattan <ruben@mobicage.com>
2016-10-19 09:05:56 +0200

    Fix navigation footer color

--- release-scripts/prepare_build.py
+++ release-scripts/prepare_build.py
@@ -763,13 +763,20 @@ public class AppConstants {
                   mc_homescreen_text='homescreen_text',
                   mc_primary_color='primary_color',
                   mc_secondary_color='secondary_color',
-                  mc_icon_color='primary_color')
+                  mc_primary_icon='primary_color')
     with open(os.path.join(path, "colors.xml"), 'r+') as f:
         s = f.read()
         for mc_color_name, color_name in colors.iteritems():
             s = re.sub('<color name="%s">(.*)</color>' % mc_color_name,
                        '<color name="%s">#%s</color>' % (mc_color_name, doc["COLORS"][color_name]),
                        s)
+
+        s = re.sub('<color name="mc_primary_color_dark">(.*)</color>',
+                   '<color name="mc_primary_color_dark">%s</color>' % colorscale(doc["COLORS"][
+                                                                                     "primary_color"],
+                                                                                 0.6),
+                   s)
+
         f.seek(0)
         f.write(s)
         f.truncate()
@@ -786,6 +793,42 @@ public class AppConstants {
         app_utils.create_background(os.path.join(APP_DIR, "build", "homescreen_footer.png"),
                                     os.path.join(SRC_RES_DIR, "drawable", "homescreen_qr_area_background.png"))
 
+
+def clamp(val, minimum=0, maximum=255):
+    if val < minimum:
+        return minimum
+    if val > maximum:
+        return maximum
+    return val
+
+
+def colorscale(hexstr, scalefactor):
+    """
+    Scales a hex string by ``scalefactor``. Returns scaled hex string.
+
+    To darken the color, use a float value between 0 and 1.
+    To brighten the color, use a float value greater than 1.
+
+    # >>> colorscale("#DF3C3C", .5)
+    #6F1E1E
+    # >>> colorscale("#52D24F", 1.6)
+    #83FF7E
+    # >>> colorscale("#4F75D2", 1)
+    #4F75D2
+    """
+
+    hexstr = hexstr.strip('#')
+
+    if scalefactor < 0 or len(hexstr) != 6:
+        return hexstr
+
+    r, g, b = int(hexstr[:2], 16), int(hexstr[2:4], 16), int(hexstr[4:], 16)
+
+    r = clamp(r * scalefactor)
+    g = clamp(g * scalefactor)
+    b = clamp(b * scalefactor)
+
+    return "#%02x%02x%02x" % (r, g, b)
 
 def encode_translation(s):
     return s.replace("\n", "\\n") \
