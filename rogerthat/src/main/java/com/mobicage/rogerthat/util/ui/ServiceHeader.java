7ef28458f6abf9b144f5c610456fa40c50d47ad4
Ruben Mattan <ruben@mobicage.com>
2017-05-08 11:52:36 +0200

    Fix #112 Service header avatar shown incorretly

--- rogerthat/src/main/java/com/mobicage/rogerthat/util/ui/ServiceHeader.java
+++ rogerthat/src/main/java/com/mobicage/rogerthat/util/ui/ServiceHeader.java
@@ -30,8 +30,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.mobicage.rogerth.at.R;
+import com.mobicage.rogerthat.MainService;
 import com.mobicage.rogerthat.plugins.messaging.BrandingMgr;
 import com.mobicage.rogerthat.util.TextUtils;
+import com.mobicage.rogerthat.util.logging.L;
+import com.mobicage.rogerthat.util.system.SafeRunnable;
 
 import in.uncod.android.bypass.Bypass;
 
@@ -39,7 +42,7 @@ import static com.mobicage.rogerthat.util.ui.ImageHelper.getRoundedCornerAvatar;
 
 public class ServiceHeader {
 
-    public static void setupNative(Context context, final BrandingMgr.BrandingResult brandingResult,
+    public static void setupNative(final MainService mainService, final BrandingMgr.BrandingResult brandingResult,
                                    final LinearLayout headerContainer) {
         final FrameLayout imagesContainer = (FrameLayout) headerContainer.findViewById(R.id.images_container);
         final ImageView logoImage = (ImageView) headerContainer.findViewById(R.id.logo_image);
@@ -58,19 +61,16 @@ public class ServiceHeader {
             messageView.setVisibility(View.GONE);
         } else {
             messageView.setVisibility(View.VISIBLE);
-            messageView.setText(TextUtils.toMarkDown(context, brandingResult.message));
+            messageView.setText(TextUtils.toMarkDown(mainService, brandingResult.message));
             messageView.setMovementMethod(LinkMovementMethod.getInstance());
         }
         logoImage.setVisibility(View.VISIBLE);
         logoImage.setImageBitmap(logo);
         if (brandingResult.avatar != null) {
-            final Bitmap avatar = BitmapFactory.decodeFile(brandingResult.avatar.getAbsolutePath());
-            avatarImage.setImageBitmap(getRoundedCornerAvatar(avatar));
-            avatarImage.setVisibility(View.VISIBLE);
             if (logoImage.getHeight() != 0) {
                 // This will be called when the branding is refreshed due to an intent, but the logo didn't change.
                 // Therefore the height is already set and the layoutChangeListener shouldn't be attached again.
-                setAvatarMargin(logoImage, imagesContainer, avatarImage);
+                setAvatarMargin(mainService, brandingResult, logoImage, imagesContainer, avatarImage);
                 return;
             }
             // When the view is drawn for the first time, calculate the correct height of the container so that the
@@ -84,20 +84,36 @@ public class ServiceHeader {
                     if (left == 0 && top == 0 && right == 0 && bottom == 0) {
                         return;
                     }
-                    setAvatarMargin(logoImage, imagesContainer, avatarImage);
+                    setAvatarMargin(mainService, brandingResult, logoImage, imagesContainer, avatarImage);
                 }
             });
         }
     }
 
-    private static void setAvatarMargin(ImageView logoImage, FrameLayout imagesContainer, ImageView avatarImage) {
-        int avatarHeight = logoImage.getHeight();
-        int halfAvatarHeight = Math.round(avatarHeight / 2);
-        imagesContainer.getLayoutParams().height = avatarHeight + halfAvatarHeight;
-        ViewGroup.MarginLayoutParams avatarMarginLayoutParams = (ViewGroup.MarginLayoutParams) avatarImage.getLayoutParams();
-        //noinspection SuspiciousNameCombination
-        avatarMarginLayoutParams.width = avatarHeight;
-        avatarMarginLayoutParams.height = avatarHeight;
-        avatarMarginLayoutParams.topMargin = avatarHeight - halfAvatarHeight;
+    private static void setAvatarMargin(MainService mainService, final BrandingMgr.BrandingResult brandingResult,
+                                        final ImageView logoImage, final FrameLayout imagesContainer,
+                                        final ImageView avatarImage) {
+        mainService.postOnUIHandler(new SafeRunnable() {
+            @Override
+            protected void safeRun() throws Exception {
+                final Bitmap avatar = BitmapFactory.decodeFile(brandingResult.avatar.getAbsolutePath());
+                avatarImage.setImageBitmap(getRoundedCornerAvatar(avatar));
+                avatarImage.setVisibility(View.VISIBLE);
+
+                int avatarHeight = logoImage.getHeight();
+                int halfAvatarHeight = Math.round(avatarHeight / 2);
+                
+                LinearLayout.LayoutParams lpImagesContainer = (LinearLayout.LayoutParams) imagesContainer.getLayoutParams();
+                lpImagesContainer.height = avatarHeight + halfAvatarHeight;
+                imagesContainer.setLayoutParams(lpImagesContainer);
+
+                ViewGroup.MarginLayoutParams avatarMarginLayoutParams = (ViewGroup.MarginLayoutParams) avatarImage.getLayoutParams();
+                //noinspection SuspiciousNameCombination
+                avatarMarginLayoutParams.width = avatarHeight;
+                avatarMarginLayoutParams.height = avatarHeight;
+                avatarMarginLayoutParams.topMargin = avatarHeight - halfAvatarHeight;
+                avatarImage.setLayoutParams(avatarMarginLayoutParams);
+            }
+        });
     }
 }
